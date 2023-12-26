afb2c089d0dadbf9269adcf5f49a6d2f90eaaf84
vsd269 <142389217+vsd269@users.noreply.github.com>
2023-12-26 17:18:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -328,7 +328,7 @@ const BLOG = {
         process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
 
   SEO_BAIDU_SITE_VERIFICATION:
-        process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || '<meta name="baidu-site-verification" content="codeva-sOmCP6WTBv" />', // Remove the value or replace it with your own google site verification code
+        process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || 'codeva-sOmCP6WTBv', // Remove the value or replace it with your own google site verification code
 
   // <---- 站点统计
 
