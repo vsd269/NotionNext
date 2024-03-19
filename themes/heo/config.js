17ca2cbda0871d2423b7096acaa00edd65e3e608
vsd269 <142389217+vsd269@users.noreply.github.com>
2024-03-20 00:54:54 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -46,8 +46,8 @@ const CONFIG = {
       img_1: 'https://tupian1014-1251479570.cos.ap-shanghai.myqcloud.com/tupian/Topaz%20Photo%20AI.webp',
       color_1: '#9ca2fb',
       title_2: 'SD',
-      img_2: 'https://tupian1014-1251479570.cos.ap-shanghai.myqcloud.com/tupian/sd-1.webp',
-      color_2: '#300463'
+      img_2: 'https://tupian1014-1251479570.cos.ap-shanghai.myqcloud.com/tupian/stable-diffusion.webp',
+      color_2: '#310463'
     },
     {
       title_1: 'chatgpt',
